85f993ed97824b52ff672615f70b9309a6f87013
Ezequiel <ezequiel.maranda@gmail.com>
2023-06-12 17:21:12 -0300

    deleted utils folder

--- components/BkgIcons/index.tsx
+++ components/BkgIcons/index.tsx
@@ -1,13 +1,4 @@
 import Image from "next/image";
-import {
-  style,
-  style2,
-  small2,
-  small3,
-  medium,
-  medium2,
-  large,
-} from "../../utils/constants";
 import js from "../../assets/js.png";
 import react from "../../assets/react.png";
 import ts from "../../assets/ts.png";
@@ -21,6 +12,16 @@ import fbse from "../../assets/firebase.png";
 import mgo from "../../assets/mongo.png";
 import gh from "../../assets/gh.png";
 import nde from "../../assets/node.png";
+
+const style: string =
+  "absolute rounded-xl bg-iconBkg p-3 pointer-events-none transition-transform ease-out duration-[3000ms]";
+const style2: string =
+  "absolute rounded-xl border border-4 border-iconBkg p-3 pointer-events-none transition-transform ease-out duration-[3000ms]";
+const small2: string = "w-12 h-12 z-0 opacity-20";
+const small3: string = "w-12 h-12 z-0 opacity-10";
+const medium: string = "w-16 h-16 z-10 opacity-30";
+const medium2: string = "w-16 h-16 z-10 opacity-10";
+const large: string = "w-20 h-20 z-20 opacity-30";
 
 interface BkgIconsProps {
   iconTransformX: {
