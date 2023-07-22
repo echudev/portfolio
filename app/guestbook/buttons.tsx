e8f47e30c700e7b0ecf75c5c136ad57532d42f67
Ezequiel <ezequiel.maranda@gmail.com>
2023-07-22 16:51:31 -0300

    form buttons refactor

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -1,6 +1,10 @@
 import { Gh4Icon, GoogleIcon } from "../components/Icons";
 import { signInWithGoogle, logoutFirebase } from "../../firebase/providers";
 
+interface BtnProps {
+  onClick: () => void;
+}
+
 export function GoogleLoginBtn() {
   const startGoogleSignIn = async () => {
     await signInWithGoogle();
@@ -22,6 +26,39 @@ export function GithubLoginBtn() {
     <button className="group flex gap-2 border rounded-sm py-1 px-2 items-center border-neutral-700 hover:border-neutral-400 bg-neutral-700 bg-opacity-60 text-neutral-200 hover:bg-neutral-800 active:scale-95 transition-all">
       <Gh4Icon />
       Github
+    </button>
+  );
+}
+
+export function ComentBtn({ onClick }: BtnProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="text-center px-3 border border-violet-600 bg-violet-700 rounded hover:bg-violet-500 transition-all"
+    >
+      comentar
+    </button>
+  );
+}
+
+export function ClearBtn({ onClick }: BtnProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="text-center px-3 rounded border border-neutral-400 text-neutral-300 hover:text-violet-500 hover:border-violet-500 transition-all"
+    >
+      borrar
+    </button>
+  );
+}
+
+export function LogoutBtn() {
+  return (
+    <button
+      onClick={logoutFirebase}
+      className="text-center px-3 text-neutral-300 hover:text-red-500 transition-all"
+    >
+      salir
     </button>
   );
 }
--- app/guestbook/form.tsx
+++ app/guestbook/form.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { FirebaseAuth } from "@/firebase/config";
 import { useState, useEffect, useRef } from "react";
-import { GithubLoginBtn, GoogleLoginBtn } from "./buttons";
-import { logoutFirebase } from "@/firebase/providers";
+import {
+  GithubLoginBtn,
+  GoogleLoginBtn,
+  ComentBtn,
+  ClearBtn,
+  LogoutBtn,
+} from "./buttons";
 
 function FormComponent() {
   const [user, setUser] = useState<Object | undefined>(undefined);
@@ -19,44 +24,21 @@ function FormComponent() {
   };
 
   useEffect(() => {
-    FirebaseAuth.onAuthStateChanged(async (user) => {
-      user ? setUser(user.providerData) : setUser(undefined);
+    FirebaseAuth.onAuthStateChanged(async (firebaseUser) => {
+      try {
+        if (firebaseUser) {
+          setUser(firebaseUser.providerData);
+        } else {
+        }
+      } catch (error) {
+        console.log(error);
+      }
     });
   }, []);
 
   return (
     <div>
-      {user ? (
-        <div className="p-3">
-          <input
-            className="rounded text-black px-1 w-full"
-            ref={userInput}
-            name="userMessage"
-            type="text"
-            placeholder=" tu mensaje..."
-          />
-          <div aria-label="bottons" className="flex mt-2 gap-2">
-            <button
-              onClick={handleSendInput}
-              className="text-center px-3 border border-violet-600 bg-violet-700 rounded hover:bg-violet-500 transition-all"
-            >
-              comentar
-            </button>
-            <button
-              onClick={handleClearInput}
-              className="text-center px-3 rounded border border-neutral-400 text-neutral-300 hover:text-violet-500 hover:border-violet-500 transition-all"
-            >
-              borrar
-            </button>
-            <button
-              onClick={logoutFirebase}
-              className="text-center px-3 text-neutral-300 hover:text-red-500 transition-all"
-            >
-              salir
-            </button>
-          </div>
-        </div>
-      ) : (
+      {!user ? (
         <>
           <div
             aria-label="login buttons"
@@ -71,6 +53,21 @@ function FormComponent() {
             * tus datos están protegidos por Google Firebase
           </p>
         </>
+      ) : (
+        <div className="p-3">
+          <input
+            className="rounded text-black px-1 w-full"
+            ref={userInput}
+            name="userMessage"
+            type="text"
+            placeholder=" tu mensaje..."
+          />
+          <div aria-label="bottons" className="flex mt-2 gap-2">
+            <ComentBtn onClick={handleSendInput} />
+            <ClearBtn onClick={handleClearInput} />
+            <LogoutBtn />
+          </div>
+        </div>
       )}
     </div>
   );
