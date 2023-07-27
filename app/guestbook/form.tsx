ed1da178558408bbfefd9b81eb705bf5444a6c9f
Ezequiel <ezequiel.maranda@gmail.com>
2023-07-27 00:28:33 -0300

    feat: empty message alert created

--- app/guestbook/form.tsx
+++ app/guestbook/form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import clsx from "clsx";
 import { signInWithGoogle, logoutFirebase } from "../../firebase/providers";
 import { doc, setDoc, collection } from "firebase/firestore";
@@ -15,6 +15,7 @@ import {
 
 function Form() {
   const { user, loading, setLoading } = useGetFirebaseAuth();
+  const [hideAlert, setHideAlert] = useState<boolean>(true);
   const userInput = useRef<HTMLInputElement>(null);
 
   const startGoogleSignIn = async () => {
@@ -27,6 +28,14 @@ function Form() {
     setLoading(true);
     // create messaje obj
     if (user && userInput.current) {
+      // check that the message is not empty
+      if (userInput.current.value.length === 0) {
+        setHideAlert(false);
+        userInput.current.style.border = "2px solid red";
+        setLoading(false);
+        return;
+      }
+      // create new message object
       const newMessage = {
         uid: user.uid,
         name: user.displayName,
@@ -34,7 +43,7 @@ function Form() {
         comment_text: userInput.current.value,
         parent_comment_id: "",
       };
-      // send doc to Firestore db
+      // set the new message on firestore DB
       const newDoc = doc(collection(db, `comments/`));
       await setDoc(newDoc, newMessage);
       // reset input
@@ -42,6 +51,13 @@ function Form() {
         userInput.current.value = "";
       }
       setLoading(false);
+    }
+  };
+
+  const hideAlertOnFocus = () => {
+    if (userInput.current) {
+      setHideAlert(true);
+      userInput.current.style.border = "";
     }
   };
 
@@ -72,11 +88,19 @@ function Form() {
               name="userMessage"
               type="text"
               placeholder=" escribe tu mensaje ..."
+              onFocus={hideAlertOnFocus}
             />
             <ComentBtn onClick={setMessageFirestore} loading={loading} />
           </div>
-          <div aria-label="bottons" className="flex mt-2 gap-2">
+          <div className="flex mt-2">
             <LogoutBtn onClick={logoutFirebase} />
+            <p
+              className={clsx("w-full text-center text-red-600 -mt-2", {
+                hidden: hideAlert,
+              })}
+            >
+              el campo no puede estar vacío!
+            </p>
           </div>
         </div>
       )}
