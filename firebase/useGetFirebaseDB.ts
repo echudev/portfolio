be479bf0f5dd913fcd643c09dcbb5797be8e9e94
Ezequiel <ezequiel.maranda@gmail.com>
2023-08-02 09:30:46 -0300

    fix: firestore onSnapshot now iterate on snap.docChanges()

--- firebase/useGetFirebaseDB.ts
+++ firebase/useGetFirebaseDB.ts
@@ -1,32 +1,34 @@
 import { collection, query, onSnapshot, orderBy, DocumentData } from "firebase/firestore";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { db } from "./config";
 
 export const useGetFirestoreDB = () => {
   const [comments, setComments] = useState<DocumentData[]>([]);
   const [loadingDB, setLoadingDB] = useState<boolean>(false)
 
-  useEffect(() => {
-    setLoadingDB(true)
-    const collectionRef = collection(db, "comments/");
-    const queryCollection = query(collectionRef, orderBy("date", "desc"));
+  const collectionRef = query(collection(db, "comments/"), orderBy("date", "desc"));
 
-    const unsubscribe = onSnapshot(queryCollection, (querySnapshot) => {
+    const unsubscribe = onSnapshot(collectionRef, snap => {
+      setLoadingDB(true)
       const newComments: DocumentData[] = [];
-      querySnapshot.forEach((doc) => {
-        let data = doc.data()
-        let id = doc.id
-        let newDoc = {... data, id }
-        newComments.push(newDoc);
+      snap.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          let data = change.doc.data()
+          let id = change.doc.id
+          let newDoc = {... data, id }
+          newComments.push(newDoc);
+        }
       });
       setComments(newComments);
       setLoadingDB(false)
     });
 
+  useEffect(() => {
+    unsubscribe
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [unsubscribe]);
 
   return { comments, loadingDB, setLoadingDB };
 };
