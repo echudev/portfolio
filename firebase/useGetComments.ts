325a2abf2b95695363e8fc1d1b41c8c9764cea3d
Ezequiel <ezequiel.maranda@gmail.com>
2023-08-02 23:49:56 -0300

    fix: fixed useGetComments loader

--- firebase/useGetComments.ts
+++ firebase/useGetComments.ts
@@ -4,21 +4,20 @@ import { db } from "./config";
 
 export const useGetFirestoreDB = () => {
   const [comments, setComments] = useState<DocumentData[]>([]);
-  const [loadingDB, setLoadingDB] = useState<boolean>(false)
+  const [loadingDB, setLoadingDB] = useState<boolean>(true)
 
   const collectionRef = query(collection(db, "comments/"), orderBy("date", "desc"));
 
     const unsubscribe = onSnapshot(collectionRef, snap => {
-      setLoadingDB(true)
       const newComments: DocumentData[] = [];
-      snap.docChanges().forEach((change) => {
-        if (change.type === "added") {
-          let data = change.doc.data()
-          let id = change.doc.id
+
+      snap.forEach((doc) => {
+          let data = doc.data()
+          let id = doc.id
           let newDoc = {... data, id }
           newComments.push(newDoc);
-        }
       });
+
       setComments(newComments);
       setLoadingDB(false)
     });
