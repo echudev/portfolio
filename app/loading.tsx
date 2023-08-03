111bde387ee1f9ec7b129cfcd13b296de83daba1
Ezequiel <ezequiel.maranda@gmail.com>
2023-08-02 23:55:12 -0300

    loader style

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div className="flex items-center justify-center">cargando...</div>;
+  return (
+    <div className="flex flex-col justify-center items-center my-20">
+      <div className="animate-spin border-4 border-x-violet-600 border-y-transparent h-5 w-5 rounded-full"></div>
+    </div>
+  );
 }
