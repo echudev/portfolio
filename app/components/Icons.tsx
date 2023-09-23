990608db28c5eb5a760874ede46c78b666bf55f7
Ezequiel <ezequiel.maranda@gmail.com>
2023-09-22 21:29:12 -0300

    feat: twitter x icon updated

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -8,6 +8,24 @@ export const TwIcon = () => {
       version="1.1"
       id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <title>twitter x</title>
+      <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+    </svg>
+  );
+};
+
+export const TwIconOld = () => {
+  return (
+    <svg
+      className="fill-slate-100 hover:fill-violet-500"
+      fill="#000000"
+      height="19px"
+      width="19px"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
       viewBox="-271 296.6 256.4 208.4"
     >
       <title>twitter</title>
